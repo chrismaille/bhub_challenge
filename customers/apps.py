e030093b4dc4e0bb56be05fddd677ababc99b171
Chris Maillefaud <chrismaille@users.noreply.github.com>
2022-06-11 01:43:48 -0300

    chore: fix race condition at startup

--- customers/apps.py
+++ customers/apps.py
@@ -1,7 +1,7 @@
 import os
 
 from django.apps import AppConfig
-from django.db import ProgrammingError
+from django.db import IntegrityError, ProgrammingError
 from loguru import logger
 
 from core import settings
@@ -36,15 +36,18 @@ class CustomersConfig(AppConfig):
     def create_local_super_user():
         from django.contrib.auth.models import User
 
-        super_user_exists = User.objects.filter(is_superuser=True).exists()
-        if settings.ENV in ["local"] and not super_user_exists:
-            logger.warning("Creating local super user.")
-            super_user = User.objects.create_superuser(
-                username=os.getenv("DJANGO_SUPERUSER_USERNAME", "admin"),
-                email=os.getenv(
-                    "DJANGO_SUPERUSER_EMAIL",
-                    f"tech@{settings.GOOGLE_SSO_ALLOWABLE_DOMAINS[0]}",
-                ),
-                password=os.getenv("DJANGO_SUPERUSER_PASSWORD", "admin"),
-            )
-            logger.info(f"Created superuser: {super_user.username}")
+        try:
+            super_user_exists = User.objects.filter(is_superuser=True).exists()
+            if settings.ENV in ["local"] and not super_user_exists:
+                logger.warning("Creating local super user.")
+                super_user = User.objects.create_superuser(
+                    username=os.getenv("DJANGO_SUPERUSER_USERNAME", "admin"),
+                    email=os.getenv(
+                        "DJANGO_SUPERUSER_EMAIL",
+                        f"tech@{settings.GOOGLE_SSO_ALLOWABLE_DOMAINS[0]}",
+                    ),
+                    password=os.getenv("DJANGO_SUPERUSER_PASSWORD", "admin"),
+                )
+                logger.info(f"Created superuser: {super_user.username}")
+        except IntegrityError:
+            logger.debug("Error while bootstrapping server. User already created?")
